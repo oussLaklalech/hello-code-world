293a2682285efd947e848eab8e29c320303e3e82
Laklalech Oussama <guest1386@telespazio.corp>
2020-01-15 15:54:45 +0100

    Add Angular Material and other CSS librairies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { FormsModule } from '@angular/forms';
 import { SectionComponent } from './section/section.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,11 @@ import { SectionComponent } from './section/section.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
