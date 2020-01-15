da32f36a556635ac2f8c9e298cd90eeec68f3fe0
Laklalech Oussama <guest1386@telespazio.corp>
2020-01-15 11:14:29 +0100

    add the article as an input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Article } from './classes/article';
+import { Section } from './classes/section';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-code-world';
+
+
+  article: Article = {
+    id: 1,
+    title: 'JAVA 8',
+    resume: 'This is a short description of Java 8',
+    sections: [
+      new Section(1, "Introduction", "introduction of java 8"),
+      new Section(2, "What's new", "We will describe all the features of the java 8"),
+      new Section(3, "Conclusion", "Conclusion of java 8")
+    ]
+  };
 }
